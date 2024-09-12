4189d9b08a0eeff645458c975edc3bab0e73b609
Doniyor-Roziqulov <doniyorruziqulov33@gmail.com>
2024-09-12 14:16:10 +0500

    Nimadir

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ const App = () => {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path="" element={<Home />} />
-                </Route>
+                <Route path="/" element={<Layout />}></Route>
             </Routes>
         </>
     );
