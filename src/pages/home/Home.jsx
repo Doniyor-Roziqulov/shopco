3d7278bc89ad896a12ac18da155747458d499b4a
Doniyor-Roziqulov <doniyorruziqulov33@gmail.com>
2024-09-12 14:40:10 +0500

    Add: home file

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
+import Dress from "@/components/dress/Dress";
+import Happy from "@/components/happy/Happy";
 import Hero from "@/components/hero/Hero";
+import Products from "@/components/products/Products";
 import React from "react";
 
 const Home = () => {
     return (
         <div>
             <Hero />
+            <Products />
+            <Dress />
+            <Happy />
         </div>
     );
 };
