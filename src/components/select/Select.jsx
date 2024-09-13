228b7c551205fe09d1e3f9ac9709ad0c87a367e3
Doniyor-Roziqulov <doniyorruziqulov33@gmail.com>
2024-09-13 20:08:05 +0500

    Add header responsive and navbar toggle

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import { Select } from "antd";
 
 const SelectComponent = () => {
-  const handleChange = (value) => {
-    console.log(`selected ${value}`);
-  };
+    const handleChange = (value) => {
+        console.log(`selected ${value}`);
+    };
 
-  const { Option } = Select;
-  return (
-    <>
-      <Select bordered={false} defaultValue="Shop" style={{ width: 94 }} onChange={handleChange}>
-        <Option value="tshirt">T-Shirt</Option>
-        <Option value="shorts">Shorts</Option>
-        <Option value="hoodies">Hoodies</Option>
-      </Select>
-    </>
-  );
+    const { Option } = Select;
+    return (
+        <>
+            <Select
+                className="text-lg"
+                bordered={false}
+                defaultValue="Shop"
+                style={{ width: 94 }}
+                onChange={handleChange}>
+                <Option value="tshirt">T-Shirt</Option>
+                <Option value="shorts">Shorts</Option>
+                <Option value="hoodies">Hoodies</Option>
+            </Select>
+        </>
+    );
 };
 
 export default SelectComponent;
