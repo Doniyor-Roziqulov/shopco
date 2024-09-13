dfca19e26d23726147e0e5a1b4e2f25d9dd772db
Doniyor-Roziqulov <doniyorruziqulov33@gmail.com>
2024-09-13 18:09:11 +0500

    Add container style

--- src/components/happy/Happy.jsx
+++ src/components/happy/Happy.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import stars from '../../assets/star.svg'
-import ticket from '../../assets/ticket.svg'
-import { Pagination } from 'antd'
+import React from "react";
+import stars from "../../assets/star.svg";
+import ticket from "../../assets/ticket.svg";
+import { Pagination } from "antd";
 
 const Happy = () => {
     // const onShowSizeChange = (current, pagesize) => {
@@ -10,56 +10,70 @@ const Happy = () => {
     const App = () => <Pagination defaultCurrent={1} total={50} />;
     return (
         <>
-            <div className='container flex flex-col gap-[40px]'>
-                <div className='flex items-center justify-between'>
-                    <h2 className='font-bold text-[48px] leading-[57px] mt-4    '>OUR HAPPY CUSTOMERS</h2>
+            <div className="container max-w-[1280px] flex flex-col gap-[40px]">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-bold text-[48px] leading-[57px] mt-4    ">
+                        OUR HAPPY CUSTOMERS
+                    </h2>
                     <Pagination align="start" defaultCurrent={1} total={50} />
-
                 </div>
-                <div className='flex gap-[20px] '>
-                    <div className='border rounded-[20px] px-[32px] py-[28px] flex flex-col gap-[18px]'> 
-                         <div className=''>
+                <div className="flex gap-[20px] ">
+                    <div className="border rounded-[20px] px-[32px] py-[28px] flex flex-col gap-[18px]">
+                        <div className="">
                             <img src={stars} alt="" />
-                         </div>
-                         <div className='flex'>
+                        </div>
+                        <div className="flex">
                             <b>Sarah M.</b>
                             <img src={ticket} alt="" />
-                         </div>
-                         <div className='flex'>
-                            <p className='leading-[22px] text-[rgb(97,96,96)]'>"I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”</p>
-                         </div>
+                        </div>
+                        <div className="flex">
+                            <p className="leading-[22px] text-[rgb(97,96,96)]">
+                                "I'm blown away by the quality and style of the
+                                clothes I received from Shop.co. From casual
+                                wear to elegant dresses, every piece I've bought
+                                has exceeded my expectations.”
+                            </p>
+                        </div>
                     </div>
-                    <div className='border rounded-[20px] px-[32px] py-[28px] flex flex-col gap-[18px]'> 
-                         <div className=''>
+                    <div className="border rounded-[20px] px-[32px] py-[28px] flex flex-col gap-[18px]">
+                        <div className="">
                             <img src={stars} alt="" />
-                         </div>
-                         <div className='flex'>
+                        </div>
+                        <div className="flex">
                             <b>Sarah M.</b>
                             <img src={ticket} alt="" />
-                         </div>
-                         <div className='flex'>
-                            <p className='leading-[22px] text-[rgb(97,96,96)]'>"I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”</p>
-                         </div>
+                        </div>
+                        <div className="flex">
+                            <p className="leading-[22px] text-[rgb(97,96,96)]">
+                                "I'm blown away by the quality and style of the
+                                clothes I received from Shop.co. From casual
+                                wear to elegant dresses, every piece I've bought
+                                has exceeded my expectations.”
+                            </p>
+                        </div>
                     </div>
-                    <div className='border rounded-[20px] px-[32px] py-[28px] flex flex-col gap-[18px]'> 
-                         <div className=''>
+                    <div className="border rounded-[20px] px-[32px] py-[28px] flex flex-col gap-[18px]">
+                        <div className="">
                             <img src={stars} alt="" />
-                         </div>
-                         <div className='flex'>
+                        </div>
+                        <div className="flex">
                             <b>Sarah M.</b>
                             <img src={ticket} alt="" />
-                         </div>
-                         <div className='flex'>
-                            <p className='leading-[22px] text-[rgb(97,96,96)]'>"I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”</p>
-                         </div>
+                        </div>
+                        <div className="flex">
+                            <p className="leading-[22px] text-[rgb(97,96,96)]">
+                                "I'm blown away by the quality and style of the
+                                clothes I received from Shop.co. From casual
+                                wear to elegant dresses, every piece I've bought
+                                has exceeded my expectations.”
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div>
-                
-                </div>
+                <div></div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Happy
+export default Happy;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,20 +3,22 @@ import Navbar from "../navbar/Navbar";
 import Baner from "../baner/Baner";
 
 const Header = () => {
-  return (
-    <>
-      <Baner />
-      <header className="py-6">
-        <div className="container flex items-center justify-between">
-          <Link className="mr-10 hover:opacity-80 active:opacity-65" to={"/"}>
-            <img src="/public/logo.svg" alt="Logo icon" />
-          </Link>
+    return (
+        <>
+            <Baner />
+            <header className="py-6">
+                <div className="container max-w-[1280px] flex items-center justify-between">
+                    <Link
+                        className="mr-10 hover:opacity-80 active:opacity-65"
+                        to={"/"}>
+                        <img src="/public/logo.svg" alt="Logo icon" />
+                    </Link>
 
-          <Navbar />
-        </div>
-      </header>
-    </>
-  );
+                    <Navbar />
+                </div>
+            </header>
+        </>
+    );
 };
 
 export default Header;
